a77668bbcce96548bf0a9b7d1903fe63bca19927
tadanokun <47693077+tadanokun@users.noreply.github.com>
2020-02-05 18:34:48 +0300

    Add files via upload

---
+++ kkomisaaan.py
@@ -0,0 +1,56 @@
+import subprocess, os, sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    install("bs4")
+try:
+    import requests
+except ImportError:
+    install("requests")
+try:
+    from PIL import Image
+except ImportError:
+    install("Pillow")
+#initialize
+url = input("enter a chapter url from mangaread.co:\n")
+print("\nPlease wait, it may take 1 minute...\n")
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+directory = str(soup.find("li", class_="active").text)[1:]
+if os.path.isdir(directory) != True:
+    os.mkdir(directory)
+pages = soup.find("div", class_="select-pagination")
+links = pages.find_all("option")
+
+#fetch the images
+n = 0
+for link in links:
+    newUrl = link["data-redirect"]
+    response = requests.get(newUrl)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img = soup.find("img", class_="wp-manga-chapter-img")
+    if n < 10: a = "00"
+    elif 9 < n < 100: a = "0"
+    else: a = ""
+    with open(directory+"/"+ "image-%s" % a + str(n) +".jpg", 'wb') as f:
+        f.write(requests.get(img["src"]).content)
+    n += 1
+
+#create a pdf
+imageList = []
+directory = directory + '/'
+for item in os.listdir(directory)[1:]:
+    imag = Image.open(directory + item)
+    imageList.append(imag)
+img1 = Image.open(directory + 'image-000.jpg')
+img1.save(directory[:-1] + '.pdf', save_all=True, append_images=imageList)
+
+#delete the pictures, keep the pdf
+for item in os.listdir(directory):
+    os.remove(directory + item)
+os.rmdir(directory)
+
+print("Done! Check your pdf located at the same directory as this file.")
